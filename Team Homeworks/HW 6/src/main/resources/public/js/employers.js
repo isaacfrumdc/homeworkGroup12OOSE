00c6732885b7c4e578254beb99e34afb1f4cbecf
David <dhla2@jhu.edu>
2021-10-15 04:26:38 -0400

    Current version of HW 6- Validation done, fields and elements added, add employer nonfunctional, no CSS

--- Team Homeworks/HW 6/src/main/resources/public/js/employers.js
+++ Team Homeworks/HW 6/src/main/resources/public/js/employers.js
@@ -5,6 +5,20 @@ function deleteEmployer(employerName) {
     ).then(res => window.location.reload = window.location.reload(true));
 }
 
+function addEmployer(employerName, employerSector, employerSummary) {
+    fetch('http://localhost:7000/employers?name=' + employerName + employerSector + employerSummary, {
+            method: 'Post',
+        }
+    ).then(res => window.location.reload = window.location.reload(true));
+}
+
+const name = document.getElementById("eName");
+const sector = document.getElementById("eSector");
+const summary = document.getElementById("eSummary");
+
+
+document.getElementById("addbtn").addEventListener("click", addEmployer.bind(this, {name, sector, summary}));
+
 let delButtons = document.querySelectorAll("li > button")
 Array.prototype.forEach.call(delButtons, function(button) {
     button.addEventListener('click', deleteEmployer.bind(null, button.id));
