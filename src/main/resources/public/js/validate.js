85e69260e9192d9c2597d1e3897d1da18c49b24f
David <dhla2@jhu.edu>
2021-10-14 23:42:26 -0400

    Removed HW 6

--- src/main/resources/public/js/validate.js
+++
@@ -1,9 +0,0 @@
-function validateUsername() {
-    const name = document.getElementById("username");
-    if (name.value.length < 1) {
-        alert("Username cannot be empty!");
-        return false;
-    } else {
-        return true;
-    }
-}
