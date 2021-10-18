d4811b50c06fc6804090760707539b252c5dfcaa
Hannah Lynn <hannah.k.lynn@gmail.com>
2021-10-17 21:56:26 -0400

    got some of validation integrated

--- Team Homeworks/HW 6/build/resources/main/public/js/employers.js
+++ Team Homeworks/HW 6/build/resources/main/public/js/employers.js
@@ -15,6 +15,19 @@ function addEmployer() {
     const name = document.getElementById("name").value;
     const sector = document.getElementById("sector").value;
     const summary = document.getElementById("summary").value;
+    if (sector.length < 2 || sector.length > 100 ){
+        //checking for alphabetical characters only not working
+        //!(sector.matches("[a-zA-Z]+"))
+        alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~!");
+        return false;
+    }
+    if (name.length < 2 || name.length > 150) {
+        //checking for alphabetical characters only not working
+        //!(name.matches("[a-zA-Z]+"))
+        alert("Employer name cannot be less than 2 characters, more than 150 characters, or include $, @, ^, %, ~!");
+        return false;
+    }
+
     if (name != null && sector != null) {
         fetch('http://localhost:7000/employers?name='+name+ '&sector=' + sector+ '&summary=' + summary,{
                 method: 'Post',
@@ -23,6 +36,7 @@ function addEmployer() {
         return true;
     }
     else {
+        alert("something else went wrong :/");
         return false;
     }
 }
